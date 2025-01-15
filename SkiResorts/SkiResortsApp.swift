dcb97e4372a224076c1baeb938d8c0ebcf8d455b
Marat-FMK <96082935+Marat-FMK@users.noreply.github.com>
2025-01-15 18:59:23 +0300

    Initial Commit

---
+++ SkiResorts/SkiResortsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiResortsApp.swift
+//  SkiResorts
+//
+//  Created by Marat Fakhrizhanov on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct SkiResortsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
